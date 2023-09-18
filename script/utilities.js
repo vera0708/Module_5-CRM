ac648cc0cb6153123ba8848b06d02b2143efccd2
Vera Deeva <verasdee@gmail.com>
2023-09-18 13:49:31 +0700

    feat: begin working with image

--- script/utilities.js
+++ script/utilities.js
@@ -12,10 +12,24 @@ export const calculateTotalSum = () => {
     return pSum.textContent;
 };
 
+export const toBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+        // функции resolve и reject создаёт сам JS
+        const reader = new FileReader();
+        reader.addEventListener('loadend', () => {
+            resolve(reader.result);
+        });
+        reader.addEventListener('error', (err) => {
+            reject(err);
+        });
+        reader.readAsDataURL(file);
+    });
+};
+
 export const currencyFormatRUB = (num) => {
     return new Intl.NumberFormat('ru-RU', {
         style: 'currency',
         currency: 'RUB',
         maximumFractionDigits: 0,
     }).format(num)
-}
+};
